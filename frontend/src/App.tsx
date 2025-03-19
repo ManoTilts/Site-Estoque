a8a0d4f5aa5177b00239ae9d4306804e66d393f2
Bobertkiller <felipe.mazzeo.barbosa@outlook.com>
2025-03-19 15:12:29 -0300

    login feito, tem que modificar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
-import Login from './pages/Login.tsx'
+import SignInSide from './pages/Login/Sign-In/SignInSide.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login/>
+      <SignInSide></SignInSide>
     </>
   )
 }
